885795f4919229bff7653bff589090eddbc4ecf8
Piszmog <piszmogcloud@gmail.com>
2019-07-31 17:08:52 -0600

    remove clients from pool, do not send message to sender, and add timestamp

--- server.go
+++ server.go
@@ -5,36 +5,51 @@ import (
 	"github.com/reiver/go-oi"
 	"github.com/reiver/go-telnet"
 	"strings"
+	"time"
 )
 
 var clients = make(map[string]handler)
 
 type handler struct {
+	id     string
 	writer telnet.Writer
 }
 
 func (handler handler) ServeTELNET(ctx telnet.Context, w telnet.Writer, r telnet.Reader) {
+	id := uuid.New().String()
 	handler.writer = w
+	handler.id = id
+	clients[id] = handler
 	var buffer [1]byte
 	p := buffer[:]
-	id := uuid.New().String()
-	clients[id] = handler
 	builder := strings.Builder{}
 	builder.WriteString(id)
 	builder.WriteString(":")
 	builder.WriteString(" ")
+	var timestamp string
 	for {
 		n, err := r.Read(p)
 		if n > 0 {
+			if len(timestamp) == 0 {
+				timestamp = time.Now().Format("2006-01-02 15:04:05 MST")
+			}
 			bytes := p[:n]
 			if bytes[0] == '\n' {
 				continue
 			} else if bytes[0] == '\r' {
+				builder.WriteString(" ")
+				builder.WriteString("(")
+				builder.WriteString(timestamp)
+				builder.WriteString(")")
 				builder.WriteByte('\n')
 				input := builder.String()
 				for _, handler := range clients {
+					if id == handler.id {
+						continue
+					}
 					oi.LongWriteString(handler.writer, input)
 				}
+				timestamp = ""
 				builder.Reset()
 				builder.WriteString(id)
 				builder.WriteString(":")
@@ -44,6 +59,7 @@ func (handler handler) ServeTELNET(ctx telnet.Context, w telnet.Writer, r telnet
 			}
 		}
 		if nil != err {
+			delete(clients, id)
 			break
 		}
 	}
