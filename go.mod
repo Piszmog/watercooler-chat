ae6ed2aa0393377430dcac1244a8064bbe85a466
Piszmog <piszmogcloud@gmail.com>
2019-07-30 23:10:23 -0600

    test out sending messages to all clients connected to server

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
-module watercooler-chat
+module github.com/piszmog/watercooler-chat
 
 go 1.12
+
+require (
+	github.com/google/uuid v1.1.1
+	github.com/reiver/go-oi v1.0.0
+	github.com/reiver/go-telnet v0.0.0-20180421082511-9ff0b2ab096e
+)
--- server.go
+++ server.go
@@ -1,5 +1,59 @@
 package main
 
+import (
+	"github.com/google/uuid"
+	"github.com/reiver/go-oi"
+	"github.com/reiver/go-telnet"
+	"strings"
+)
+
+var clients = make(map[string]handler)
+
+type handler struct {
+	writer telnet.Writer
+}
+
+func (handler handler) ServeTELNET(ctx telnet.Context, w telnet.Writer, r telnet.Reader) {
+	handler.writer = w
+	var buffer [1]byte
+	p := buffer[:]
+	id := uuid.New().String()
+	clients[id] = handler
+	builder := strings.Builder{}
+	builder.WriteString(id)
+	builder.WriteString(":")
+	builder.WriteString(" ")
+	for {
+		n, err := r.Read(p)
+		if n > 0 {
+			bytes := p[:n]
+			if bytes[0] == '\n' {
+				continue
+			} else if bytes[0] == '\r' {
+				builder.WriteByte('\n')
+				input := builder.String()
+				for _, handler := range clients {
+					oi.LongWriteString(handler.writer, input)
+				}
+				builder.Reset()
+				builder.WriteString(id)
+				builder.WriteString(":")
+				builder.WriteString(" ")
+			} else {
+				builder.Write(bytes)
+			}
+		}
+		if nil != err {
+			break
+		}
+	}
+}
+
 func main() {
-	// TODO
+	var handler = handler{}
+	err := telnet.ListenAndServe(":5555", handler)
+	if nil != err {
+		//@TODO: Handle this error better.
+		panic(err)
+	}
 }
