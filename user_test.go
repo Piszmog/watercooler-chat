84bf3eda4f9f89acefca91ef8816a7348f78f3f4
Piszmog <piszmogcloud@gmail.com>
2019-08-06 16:58:53 -0600

    add test for simple user connection

--- user_test.go
+++ user_test.go
@@ -2,10 +2,46 @@ package main
 
 import (
 	"bytes"
+	"github.com/reiver/go-telnet"
+	"strings"
 	"testing"
 )
 
 func TestChatUser_ServeTELNET(t *testing.T) {
+	server = CreateServer()
+	//
+	// reset
+	//
+	defer func() {
+		server = CreateServer()
+	}()
+	user := ChatUser{}
+	reader := strings.NewReader("Tester\n\rTest Room\n\r-q\n\r")
+	var b bytes.Buffer
+	user.ServeTELNET(telnet.NewContext(), &b, reader)
+	expectedServerMessages := `What is your Name?
+Existing rooms:
+
+What room would you like to enter (if room is not listed, room will be created)? 
+Users currently in the room:
+
+Available commands:
+-r ${room Name} -- change to the specified room. Creates room if doesn't exist
+-b ${user Name} -- to block messages from the specified user
+-u ${user Name} -- to Unblock messages from the specified user
+-lr             -- to list all existing rooms
+-lr             -- to list all users in the current room
+-lr             -- to list all users currently blocked
+-q              -- to quit the chat
+-h              -- to list all available commands
+
+Tester has entered
+Welcome to room Test Room! You may begin chatting with the users.
+Quiting...
+`
+	if b.String() != expectedServerMessages {
+		t.Fatal("server did not write the expected format to the user")
+	}
 }
 
 func TestChatUser_ReceiveMessage(t *testing.T) {
